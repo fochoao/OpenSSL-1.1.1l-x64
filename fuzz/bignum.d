cc63e61b366e2631bcfe9e97270e5af6cbcf7082
Fernando Ochoa O <fochoao@pm.me>
2021-09-08 07:22:32 -0500

    OpenSSL 1.1.1l AMD64 Windows.
    OpenSSL 1.1.1l AMD64 Windows most complete release.

---
+++ fuzz/bignum.d
@@ -0,0 +1,41 @@
+bignum.c
+Note: including file: C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\stdio.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt.h
+Note: including file:   C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\vcruntime.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\sal.h
+Note: including file:     C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\concurrencysal.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\vadefs.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_wstdio.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_stdio_config.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\include\openssl/bn.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/e_os2.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/opensslv.h
+Note: including file:   C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\stdint.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/ossl_typ.h
+Note: including file:   C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\limits.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/crypto.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\stdlib.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_malloc.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_search.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\stddef.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_wstdlib.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\time.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_wtime.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/safestack.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/stack.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/cryptoerr.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/symhacks.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/bnerr.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\include\openssl/err.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/bio.h
+Note: including file:   C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\stdarg.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/bioerr.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/lhash.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\errno.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\fuzz\fuzzer.h
+..\fuzz\bignum.c(65): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+..\fuzz\bignum.c(67): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+..\fuzz\bignum.c(68): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
---
+++ test/ectest.d
@@ -0,0 +1,99 @@
+ectest.c
+Note: including file: C:\cpython\externals\openssl-1.1.1l\include\internal/nelem.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\test\testutil.h
+Note: including file:  C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\stdarg.h
+Note: including file:   C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\vcruntime.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\sal.h
+Note: including file:     C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\concurrencysal.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\vadefs.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/err.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/e_os2.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/opensslv.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\stdint.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\stdio.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_wstdio.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_stdio_config.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\stdlib.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_malloc.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_search.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\stddef.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_wstdlib.h
+Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include\limits.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/ossl_typ.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/bio.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/crypto.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\time.h
+Note: including file:      C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\corecrt_wtime.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/safestack.h
+Note: including file:      C:\cpython\externals\openssl-1.1.1l\include\openssl/stack.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/cryptoerr.h
+Note: including file:      C:\cpython\externals\openssl-1.1.1l\include\openssl/symhacks.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/bioerr.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/lhash.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\errno.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/bn.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/bnerr.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\include\openssl/ec.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/asn1.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/asn1err.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/ecerr.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\include\openssl/engine.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/rsa.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/rsaerr.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/dsa.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/dh.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/dherr.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/dsaerr.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/rand.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/randerr.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/ui.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/pem.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/evp.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/evperr.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/objects.h
+Note: including file:      C:\cpython\externals\openssl-1.1.1l\include\openssl/obj_mac.h
+Note: including file:      C:\cpython\externals\openssl-1.1.1l\include\openssl/objectserr.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/x509.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/buffer.h
+Note: including file:      C:\cpython\externals\openssl-1.1.1l\include\openssl/buffererr.h
+Note: including file:      C:\Program Files (x86)\Windows Kits\10\Include\10.0.22000.0\ucrt\sys/types.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/sha.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/x509err.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/x509_vfy.h
+Note: including file:      C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file:     C:\cpython\externals\openssl-1.1.1l\include\openssl/pkcs7.h
+Note: including file:      C:\cpython\externals\openssl-1.1.1l\include\openssl/pkcs7err.h
+Note: including file:    C:\cpython\externals\openssl-1.1.1l\include\openssl/pemerr.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\include\openssl/uierr.h
+Note: including file:  C:\cpython\externals\openssl-1.1.1l\include\openssl/engineerr.h
+Note: including file:   C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\include\openssl/obj_mac.h
+Note: including file: C:\cpython\externals\openssl-1.1.1l\tmp64dll\include\openssl/opensslconf.h
+..\test\ectest.c(631): warning C4267: 'return': conversion from 'size_t' to 'int', possible loss of data
+..\test\ectest.c(1733): warning C4267: 'function': conversion from 'size_t' to 'const int', possible loss of data
+..\test\ectest.c(1750): warning C4267: 'function': conversion from 'size_t' to 'const int', possible loss of data
+..\test\ectest.c(2191): warning C4267: 'function': conversion from 'size_t' to 'const int', possible loss of data
+..\test\ectest.c(2195): warning C4267: 'function': conversion from 'size_t' to 'const int', possible loss of data
+..\test\ectest.c(2208): warning C4267: 'function': conversion from 'size_t' to 'const int', possible loss of data
+..\test\ectest.c(2212): warning C4267: 'function': conversion from 'size_t' to 'const int', possible loss of data
+..\test\ectest.c(2244): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+..\test\ectest.c(2255): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+..\test\ectest.c(2256): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+..\test\ectest.c(2258): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+..\test\ectest.c(2259): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
+..\test\ectest.c(2260): warning C4267: 'function': conversion from 'size_t' to 'int', possible loss of data
